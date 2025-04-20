c357ca237beac56270d0b92d80ec55176e78da2f
Charly <charlygaf@gmail.com>
2025-04-20 18:23:16 -0300

    added tooltip

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,8 +6,19 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import { Vote, Voter } from "../../types";
-
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -19,6 +30,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { toast } from "sonner";
+import { InfoIcon } from "lucide-react";
 
 const formSchema = z.object({
   voterId: z.string().min(1, "Campo requerido"),
@@ -52,26 +64,31 @@ export default function HomePage() {
         candidateId: data.candidateId,
       });
 
-      toast(<div>
-        <h1 className="text-lg font-bold">Voto enviado</h1>
-        <p>Gracias por participar en la votación</p>
-      </div>)
+      toast(
+        <div>
+          <h1 className="text-lg font-bold">Voto enviado</h1>
+          <p>Gracias por participar en la votación</p>
+        </div>
+      );
       setValue("candidateId", "");
       reset();
     } catch (error: unknown) {
-      if (error instanceof AxiosError) {const errorMessage = (() => {
-        try {
-          const parsed = JSON.parse(error.request.response);
-          return parsed.message || "Error desconocido";
-        } catch {
-          return "Error desconocido";
-        }
-      })();
-      
-      toast(<div>
-        <h1 className="text-lg font-bold">Error al enviar voto </h1>
-        <p>{errorMessage}</p>
-      </div> );
+      if (error instanceof AxiosError) {
+        const errorMessage = (() => {
+          try {
+            const parsed = JSON.parse(error.request.response);
+            return parsed.message || "Error desconocido";
+          } catch {
+            return "Error desconocido";
+          }
+        })();
+
+        toast(
+          <div>
+            <h1 className="text-lg font-bold">Error al enviar voto </h1>
+            <p>{errorMessage}</p>
+          </div>
+        );
         console.error(error);
       }
       console.error(error);
@@ -82,54 +99,72 @@ export default function HomePage() {
 
   return (
     <div className="flex justify-center mt-20 px-4">
-      <Card className="w-full max-w-md">
-        <CardHeader>
-          <CardTitle className="text-2xl">Votación</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-            <div className="space-y-1">
-              <Label htmlFor="voterId" style={{ fontFamily: "Open Sans" }}>
-                Cédula de Identidad
-              </Label>
-              <Input id="voterId" {...register("voterId")} />
-              {errors.voterId && (
-                <p className="text-sm text-red-500">{errors.voterId.message}</p>
-              )}
-            </div>
+      <TooltipProvider>
+        <Card className="w-full max-w-md">
+          <CardHeader>
+            <CardTitle className="text-2xl">Votación</CardTitle>
+            <CardDescription>
+              Las cédulas de identidad deben ser válidas y no haber votado
+              previamente.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+              <div className="space-y-1">
+                <Label htmlFor="voterId" style={{ fontFamily: "Open Sans" }}>
+                  Cédula de Identidad
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <InfoIcon width={13} strokeWidth={2} height={13} />
+                    </TooltipTrigger>
+                    <TooltipContent className="bg-white text-black p-5">
+                      <p className="text-sm">
+                        El seeder contiene cédulas de identidad con el formato:
+                        DOC + un digito del 1 al 10
+                      </p>
+                    </TooltipContent>
+                  </Tooltip>
+                </Label>
+                <Input id="voterId" {...register("voterId")} />
+                {errors.voterId && (
+                  <p className="text-sm text-red-500">
+                    {errors.voterId.message}
+                  </p>
+                )}
+              </div>
 
-            <div className="space-y-1 w-full">
-              <Label htmlFor="candidateId">Seleccionar candidato</Label>
-              <Select
-                value={watch("candidateId") || ""}
-                onValueChange={(value) => setValue("candidateId", value)}
-              >
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Selecciona un candidato" />
-                </SelectTrigger>
-                <SelectContent>
-                  {candidates.map((c) => (
-                    <SelectItem key={c.document} value={c.document}>
-                      {c.name} {c.lastName}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              {errors.candidateId && (
-                <p className="text-sm text-red-500">
-                  {errors.candidateId.message}
-                </p>
-              )}
-            </div>
+              <div className="space-y-1 w-full">
+                <Label htmlFor="candidateId">Seleccionar candidato</Label>
 
-            <Button type="submit" className="w-full">
-              Votar
-            </Button>
-          </form>
+                <Select
+                  value={watch("candidateId") || ""}
+                  onValueChange={(value) => setValue("candidateId", value)}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Selecciona un candidato" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {candidates.map((c) => (
+                      <SelectItem key={c.document} value={c.document}>
+                        {c.name} {c.lastName}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                {errors.candidateId && (
+                  <p className="text-sm text-red-500">
+                    {errors.candidateId.message}
+                  </p>
+                )}
+              </div>
 
-       
-        </CardContent>
-      </Card>
+              <Button type="submit" className="w-full">
+                Votar
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </TooltipProvider>
     </div>
   );
 }
